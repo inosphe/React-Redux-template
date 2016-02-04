39bcec22d1ab70349e868915270d757526090870
inosphe <inosphe@gmail.com>
2016-02-05 02:09:50 +0900

    add global function & require module

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 global.__base = __dirname;
+global.__src = __dirname + '/src';
+
 global.getConfig = function(name){
 	return require(__dirname + '/config/' + name);
 }
+
+require('app-module-path').addPath(__src);
 
 var app = require('./src/app/app')({
 	rootdir: __dirname
